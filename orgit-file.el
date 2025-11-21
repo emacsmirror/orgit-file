2810e6d000386124662745eaecdbe593732ccc01
gggion <gggion123@gmail.com>
2025-11-21 12:31:40 -0300

    Add license text and magit dependency
    * orgit-file.el: Add full GPL-3.0-or-later license text to file header.
    Format Package-Requires with one dependency per line for readability.
    Add explicit magit dependency (version 4.3) to Package-Requires;
    previously relied on dependency through orgit.

--- orgit-file.el
+++ orgit-file.el
@@ -6,9 +6,27 @@
 ;; Keywords: hypermedia vc
 
 ;; Package-Version: 0.1.0
-;; Package-Requires: ((emacs "29.1") (compat "30.1") (orgit "2.0") (org "9.7"))
+;; Package-Requires: (
+;;     (emacs  "29.1")
+;;     (compat "30.1")
+;;     (magit   "4.3")
+;;     (org     "9.7")
+;;     (orgit   "2.0"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
+
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation, either version 3 of the License,
+;; or (at your option) any later version.
+;;
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
